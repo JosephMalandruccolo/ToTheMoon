d6357c68712ff7b71f88f0f44efc191a121cb16f
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-04-16 17:17:18 -0500

    Add credits page view controller

---
+++ ToTheMoon/CreditsPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreditsPageViewController.h
+//  ToTheMoon
+//
+//  Created by Joseph Malandruccolo on 4/16/13.
+//  Copyright (c) 2013 Joseph Malandruccolo. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+@interface CreditsPageViewController : PageViewController
+
+@end
---
+++ ToTheMoon/CreditsPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreditsPageViewController.m
+//  ToTheMoon
+//
+//  Created by Joseph Malandruccolo on 4/16/13.
+//  Copyright (c) 2013 Joseph Malandruccolo. All rights reserved.
+//
+
+#import "CreditsPageViewController.h"
+
+@interface CreditsPageViewController ()
+
+@end
+
+@implementation CreditsPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
